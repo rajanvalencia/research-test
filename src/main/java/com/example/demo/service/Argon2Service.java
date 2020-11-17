4c3f2264f0b36d8d30aa016e9a188ec637d7dd75
rajanvalencia <rajan.valencia@au.com>
2020-11-18 05:30:23 +0900

    minor changes

--- src/main/java/com/example/demo/service/Argon2Service.java
+++ src/main/java/com/example/demo/service/Argon2Service.java
@@ -5,24 +5,25 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 
 @Service
 public class Argon2Service {
 	
 	public static Map<String, Object> generate(String passwordToHash) {
-
-		Argon2PasswordEncoder encoder = 
-				new Argon2PasswordEncoder(21 /*salt  length*/, 128 /*hash length*/, 1 /*parallelism*/, 1 << 12 /* memory */, 3 /*iteration*/);
-
-		Map<String, Object> results = new HashMap<>();
 		
+		Argon2 argon2 = Argon2Factory.create();
+
 		Instant start = Instant.now();
-        String hash = encoder.encode(passwordToHash);
+		String hash = argon2.hash(22 /* iterations */, 65536 /* memory */, 1 /* parallelism */, passwordToHash.getBytes() /* salt */);
         Instant end = Instant.now();
         Long time = Duration.between(start, end).toNanos();
         Long time1 = Duration.between(start, end).toMillis();
+        
+        Map<String, Object> results = new HashMap<>();
         results.put("time", time + " ns");
         results.put("time-ms", time1 + " ms");
         results.put("hash", hash);
